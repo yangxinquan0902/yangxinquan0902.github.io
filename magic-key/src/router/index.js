789336143890d802847de8a6c0120123d3ebf7c3
yangxinquan0902 <yangxinquan0902@163.com>
2020-12-08 20:03:12 +0800

    ffff

--- magic-key/src/router/index.js
+++ magic-key/src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -9,7 +10,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
+  mode: 'hash',
   routes
 })
 
---
+++ magic-key/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    publicPath: './',
+    devServer: {
+        open: true,
+        inline: true,
+        port: 9999, // 端口
+        https: false, // 启用https
+    }
+};
